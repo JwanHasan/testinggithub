4c9e69fb44e0e6be93528942bd6814bd94a6b2c4
JwanHasan <cloudcaelum22@outlook.com>
2024-11-22 13:35:23 +0100

    testing-2

--- testing/src/Test3.java
+++ testing/src/Test3.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
+
 public class Test3
 {
+  public static void main(String[] args)
+  {
+    System.out.println("can you see me ");
+    Scanner scan=new Scanner(System.in);
+    String answer=scan.nextLine();
+    System.out.println(answer);
+  }
 }
